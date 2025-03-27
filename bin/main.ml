e72c122049d7be79544bc2d944d63ad420d3c55a
Augustine Yang <yang_yuanzhao@whu.edu.cn>
2025-03-27 22:29:34 +0800

    Task 2: Implement basic evaluation for if statement

--- bin/main.ml
+++ bin/main.ml
@@ -34,13 +34,13 @@ let parse s : expr =
 
 (* check if an expression is a value (i.e., fully evaluated) *)
 let is_value : expr -> bool = function
-  | Int _ -> true
-  | Binop _ -> false
+  | Int _ | Bool _ -> true
+  | Binop _ | If _ | _ -> false
 
 
 (* takes a single step of evaluation of [e] *)
 let rec step : expr -> expr = function
-  | Int _ -> failwith "Does not step on a number"
+  | Int _ | Bool _ -> failwith "Does not step on a number"
 
   (* No need for further stepping if both sides are already values *)
   | Binop (binop, e1, e2) when is_value e1 && is_value e2 -> 
@@ -52,6 +52,16 @@ let rec step : expr -> expr = function
   (* Leftmost step for binop *)
   | Binop (binop, e1, e2) -> Binop (binop, step e1, e2)
 
+  | If (Bool true, e2, _) -> e2
+  
+  | If (Bool false, _, e3) -> e3
+
+  | If (Int _, _, _) -> failwith "Condition must be a boolean"
+
+  | If (e1, e2, e3) -> If (step e1, e2, e3)
+
+  | _ -> failwith "Not Implemented"
+
 
 (* implement the primitive operation [v1 binop v2].
    Requires: [v1] and [v2] are both values. *)
@@ -61,6 +71,7 @@ and step_binop binop v1 v2 = match binop, v1, v2 with
   | Mul, Int a, Int b -> Int (a * b)
   | Div, Int a, Int b when b <> 0 -> Int (a / b)
   | Div, Int _, Int 0 -> failwith "Division by zero"
+  | Leq, Int a, Int b -> Bool (a <= b)
   | _ -> failwith "Operator and operand type mismatch"
 
 
@@ -76,8 +87,9 @@ let interp (s : string) : string =
 
 
 let rec eval_big (e : expr) : expr = match e with
-  | Int _ -> e
+  | Int _ | Bool _ -> e
   | Binop (binop, e1, e2) -> eval_bop binop e1 e2
+  | If (e1, e2, e3) -> eval_if e1 e2 e3
 
 and eval_bop binop e1 e2 = match binop, eval_big e1, eval_big e2 with
   | Add, Int a, Int b -> Int (a + b)
@@ -85,23 +97,30 @@ and eval_bop binop e1 e2 = match binop, eval_big e1, eval_big e2 with
   | Mul, Int a, Int b -> Int (a * b)
   | Div, Int a, Int b when b <> 0 -> Int (a / b)
   | Div, Int _, Int 0 -> failwith "Division by zero"
+  | Leq, Int a, Int b -> Bool (a <= b)
   | _ -> failwith "Operator and operand type mismatch"
+
+and eval_if e1 e2 e3 = match eval_big e1 with
+  | Bool true -> eval_big e2
+  | Bool false -> eval_big e3
+  | _ -> failwith "Condition must be a boolean"
+
 
 let interp_big (s : string) : string = 
   s |> parse |> eval_big |> string_of_expr
   
 let () =
-  let filename = "test/simpl_test1.in" in
-  (* let filename = "test/simpl_test2.in" in *)
+  (* let filename = "test/simpl_test1.in" in *)
+  let filename = "test/simpl_test2.in" in
   let in_channel = open_in filename in
   let file_content = really_input_string in_channel (in_channel_length in_channel) in
   close_in in_channel;
 
-  (* let res = interp file_content in
+  let res = interp file_content in
   Printf.printf "Result of interpreting %s:\n%s\n\n" filename res;
 
   let res = interp_big file_content in
-  Printf.printf "Result of interpreting %s with big-step model:\n%s\n\n" filename res; *)
+  Printf.printf "Result of interpreting %s with big-step model:\n%s\n\n" filename res;
 
   let ast = parse file_content in 
   Printf.printf "AST: %s\n" (string_of_expr ast);
